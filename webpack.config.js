c6f4c382af137289f139599f0bf6248fbcc17aa8
Sif <sif_aagaard_svendsen@hotmail.com>
2021-12-06 13:12:05 +0100

    finished videos with updated version of webpack
    the 'live server' function does not work as described in the tutorial it should. Did not feel comfortable using the old version, due to error messages, but could only get the build function to work right with the new version

--- webpack.config.js
+++ webpack.config.js
@@ -21,15 +21,21 @@ module.exports = {
         static: './dist',
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
             }
-        }]
+        ]
     }
 };
